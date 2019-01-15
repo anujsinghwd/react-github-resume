ea4d8730a18ea6d76cac296b788fe0cfcb6e8da7
anujsinghwd <anuj.s@mapmyindia.com>
2019-01-15 17:46:19 +0530

    Updates

---
+++ precache-manifest.f2261c5f293b187124f8e97f72e40b09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86b54a8d7c70a59fa386",
+    "url": "/react-github-resume/static/js/runtime~main.86b54a8d.js"
+  },
+  {
+    "revision": "899df09288299ed5273a",
+    "url": "/react-github-resume/static/js/main.899df092.chunk.js"
+  },
+  {
+    "revision": "7927ab9101b451d0790b",
+    "url": "/react-github-resume/static/js/1.7927ab91.chunk.js"
+  },
+  {
+    "revision": "899df09288299ed5273a",
+    "url": "/react-github-resume/static/css/main.860130b2.chunk.css"
+  },
+  {
+    "revision": "7927ab9101b451d0790b",
+    "url": "/react-github-resume/static/css/1.7fd86a94.chunk.css"
+  },
+  {
+    "revision": "c5e3c162930dbe1813bac2db040d4785",
+    "url": "/react-github-resume/index.html"
+  }
+];
